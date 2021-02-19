9b09a538ef58abad94b80048249a7acc711ad901
Ramya.U <ramyayadav2016@gmail.com>
2021-02-19 14:39:58 +0530

    [Ramya] Add . Added Db context and connection string

---
+++ ExpressoAPI/Data/ExpressDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ExpressoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressoAPI.Data
+{
+    public class ExpressDbContext : DbContext
+    {
+        public DbSet<Menu> Menus {get; set; }
+
+        public DbSet<Reservation> Reservation{ get; set; }
+        }
+}
--- ExpressoAPI/Startup.cs
+++ ExpressoAPI/Startup.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using ExpressoAPI.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +28,9 @@ namespace ExpressoAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ExpressDbContext>(option =>
+            option.UseSqlServer(@"Data Source=RAMYA\SQLEXPRESS;Initial Catalog=ExpressoAPI;Integrated Security=True;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
